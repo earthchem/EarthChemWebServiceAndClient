84f119326acb417c4677de4d93ffffcd3f7d1d14
lulin-song <Lulin.Song@gmail.com>
2016-10-07 10:22:45 -0400

    Counting monthly or dayly download information

---
+++ WebClients/StatisticsPlots/DownloadCounts.php
@@ -0,0 +1,15 @@
+<?php
+/* This script will draw PetDB download purpose pie chart on fly
+* @Author: Lulin Song created on Sept. 14, 2016
+*
+*/
+require_once 'PetDBStatsPlot.php';
+date_default_timezone_set('America/New_York');
+$nextyear  = mktime(0, 0, 0, date("m"),   date("d"),   date("Y")-1);
+$currentTime  = mktime(0, 0, 0, date("m"),   date("d"),   date("Y") -0);
+$monthOneYearBefore = date('Y-m-d', $nextyear);
+$monthCurrentYear = date('Y-m-d', $currentTime);
+
+PetDBStatsPlot::getMonthlyIPAndDownloadCountsFromFile();
+PetDBStatsPlot::getDaylyIPAndDownloadCountsFromFile();
+?>
