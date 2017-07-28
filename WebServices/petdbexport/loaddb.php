4e09e5e039d007456101df656e9fddbeb4749ae3
Stephen Richard <steve.richard@ldeo.columbia.edu>
2017-07-28 09:10:07 -0400

    add comment

--- WebServices/petdbexport/loaddb.php
+++ WebServices/petdbexport/loaddb.php
@@ -5,6 +5,7 @@ include_once "ez_sql_core.php";
 //include_once "ez_sql_postgresql.php";
 include_once "ez_sql_oracle8_9.php";
 
+//put in correct connection information
 $db = new ezSQL_oracle8_9('dbname','password','dbpath','iiillll');
 
 ?>
