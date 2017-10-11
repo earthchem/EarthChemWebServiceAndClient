9e97f49610c0d478df9cb58aebd1766cb4ce57ac
lulin-song <lulin.song@gmail.com>
2017-10-11 15:15:31 -0400

    Fixed a file name

--- WebClients/StatisticsPlots/ECLStatsPlot.php
+++ WebClients/StatisticsPlots/ECLStatsPlot.php
@@ -206,7 +206,7 @@ class ECLStatsPlot extends WebClient
 
     static public function getDaylyIPAndDownLoadCountsFromFile()
     {
-        $myFile = fopen("petdb_download_feedback.csv","r");
+        $myFile = fopen("earthchem_library_downloads_20171010.csv","r");
         $data=null;
         $idx=0;
         $myline = fgets($myFile); //skip first line which is column header
