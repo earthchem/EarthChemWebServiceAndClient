2210f4c79353fb513408d538e445756ccc6f4216
Lulin Song <Lulin.Song@gmail.com>
2018-01-12 11:50:39 -0500

    Re-implemented PieChart for ECL by using data from database through web service instead of static file

--- WebClients/StatisticsPlots/ECLStatsPieChart.php
+++ WebClients/StatisticsPlots/ECLStatsPieChart.php
@@ -4,24 +4,20 @@
 *
 */
 require_once 'ECLStatsPlot.php';
-date_default_timezone_set('America/New_York');
-$nextyear  = mktime(0, 0, 0, date("m"),   date("d"),   date("Y")-1);
-$currentTime  = mktime(0, 0, 0, date("m"),   date("d"),   date("Y") -0);
-$monthOneYearBefore = date('Y-m-d', $nextyear);
-$monthCurrentYear = date('Y-m-d', $currentTime);
 
-$pieChartData = json_decode( ECLStatsPlot::getPieChartDataFromFile());
+$pieChartData = json_decode( ECLStatsPlot::getPieChartData());
 
 foreach ($pieChartData as $index => $value )
 {
-  if($index =='education') $eduCnt = $value;
-  if($index =='research') $resCnt = $value;
-  if($index =='other') $otherCnt = $value;
-  if($index =='null') $nullCnt = $value;
-  if($index =='commercial') $commerCnt = $value;
-  if($index =='total') $totalCnt = $value;
-  //if($index =='emailRatio') $emailr = intval($value*100);
-  if($index =='uniqueip') $ipCnt = $value;
+  if(strtolower($index) =='education') $eduCnt = $value;
+  if(strtolower($index) =='research') $resCnt = $value;
+  if(strtolower($index) =='other') $otherCnt = $value;
+  if(strtolower($index) =='null') $nullCnt = $value;
+  if(strtolower($index) =='commercial use') $commerCnt = $value;
+  if(strtolower($index) =='total') $totalCnt = $value;
+  if(strtolower($index) =='uniqueip') $ipCnt = $value;
+  if(strtolower($index) =='start_date') $start_date = $value;
+  if(strtolower($index) =='end_date') $end_date = $value;
 }
 
 ?>
@@ -43,7 +39,7 @@ foreach ($pieChartData as $index => $value )
         ]);
 
         var options = {
-          title: 'ECL Download Statistics (Apr 19, 2012 to Oct 31, 2017)\n* <?=$totalCnt?> dataset downloads\n* <?=$ipCnt?> unique IP addresses',
+          title: 'ECL Download Statistics (<?= $start_date ?> to <?= $end_date?>)\n* <?=$totalCnt?> dataset downloads\n* <?=$ipCnt?> unique IP addresses',
           titleTextStyle: {'color':'#893d12'},
           is3D: true,
           colors: ['#e6693e', '#6a88c1', '#8ff7b6', '#f3b49f','#835C3B']
