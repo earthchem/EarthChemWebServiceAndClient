dd86f7b6727ad2e65692d85c2f0e08baed30063f
SeanSCao <ssc543@nyu.edu>
2023-03-23 18:16:39 -0400

    update ecp chart sizing

--- WebClients/StatisticsPlots/ECPCitationStatsPieChart.php
+++ WebClients/StatisticsPlots/ECPCitationStatsPieChart.php
@@ -56,7 +56,7 @@ $totalCnt = $pieData->TOTAL->citations;
     </script>
   </head>
   <body>
-    <div style="max-width: 100%; padding-top: 66.66%; position: relative; width: 100%;">
+    <div style="max-width: 100%; padding-top: 50%; position: relative; width: 100%;">
       <div id="piechart" style="position: absolute; top: 0; left: 0;width: 100%; height: 100%;"></div>
     </div>
   </body>
--- WebClients/StatisticsPlots/ECPSampleStatsPieChart.php
+++ WebClients/StatisticsPlots/ECPSampleStatsPieChart.php
@@ -101,7 +101,10 @@ $totalChemical = $pieData->TOTAL->analyses;
     </script>
   </head>
   <body>
-      <div id="piechart" style="width: 900px; height: 500px;"></div>
+    <div style="max-width: 100%; padding-top: 50%; position: relative; width: 100%;">
+      <div id="piechart" style="position: absolute; top: 0; left: 0;width: 100%; height: 100%;"></div>
+    </div>
   </body>
+
 
 </html>
